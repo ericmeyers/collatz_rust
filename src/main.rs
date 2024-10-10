9025cef2e870e003c59b88c15eca2dcd2cb2a5b7
Eric Meyers <etm@ericmeyers.com>
2024-10-10 17:51:21 -0400

    Switch to MatPlotLib

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@
 use itertools::iterate;
 use itertools::Itertools;
 
-use plotly::layout::{Layout, Axis};
-use plotly::{Plot, Scatter};
-use plotly::common::Mode;
+//use plotly::layout::{Layout, Axis};
+//use plotly::{Plot, Scatter};
+//use plotly::common::Mode;
+
+
+use plotpy::{linspace, Curve, Plot, StrError};
 
 
 fn collatz_next(n: &i64) -> i64 {
@@ -26,10 +29,30 @@ fn collatz_length(n: &i64) -> i64 {
 
 
 
-fn main() {
-    let xs:Vec<i64> = (1..=10000).collect();
+fn main() -> Result<(), StrError>  {
+    let xs:Vec<i64> = (1..=5000000).collect();
     let ys:Vec<_> = xs.iter().map(collatz_length).collect();
 
+    let mut curve = Curve::new();
+    curve
+        .set_label("MatPlotLib Test")
+ //       .set_line_alpha(0.8)
+ //       .set_line_color("#5f9cd8")
+        .set_line_style("None")
+ //       .set_marker_color("#eeea83")
+        .set_marker_every(1)
+ //       .set_marker_line_color("#da98d1")
+        .set_marker_line_width(2.5)
+        .set_marker_size(2.0)
+        .set_marker_style("o");
+
+    curve.draw(&xs, &ys);
+    let mut plot = Plot::new();
+    plot.add(&curve);
+    let _ = plot.show("myscript");
+    Ok(())
+
+/*
     let mut plot = Plot::new();
     plot.set_layout(Layout::new()
                     .title("Collatz Conjecture")
@@ -41,7 +64,5 @@ fn main() {
     let trace = Scatter::new(xs,ys).mode(Mode::Markers);
     plot.add_trace(trace);
     plot.show();
-
-
-
+*/
 }
