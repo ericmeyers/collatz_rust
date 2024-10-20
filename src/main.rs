a0199e48276154bb1788b9e5f9f4ba475046c050
Eric Meyers <etm@ericmeyers.com>
2024-10-20 08:46:38 -0400

    played with some Rust profiling tools. Very cool

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,47 @@
-
 use itertools::iterate;
 use itertools::Itertools;
 
-//use plotly::layout::{Layout, Axis};
-//use plotly::{Plot, Scatter};
-//use plotly::common::Mode;
-
-
-use plotpy::{linspace, Curve, Plot, StrError};
-
+use plotpy::{Curve, Plot, StrError};
 
 fn collatz_next(n: &i64) -> i64 {
-
-   if n % 2 == 0 {
-      n / 2
-   }
-   else {
-      3 * n + 1
-   }
-
+    if n % 2 == 0 {
+        n / 2
+    } else {
+        3 * n + 1
+    }
 }
 
 fn collatz_length(n: &i64) -> i64 {
-
-    let v: Vec<_> = iterate(*n, collatz_next).take_while_inclusive(|&n| n > 1).collect();
+    let v: Vec<_> = iterate(*n, collatz_next)
+        .take_while_inclusive(|&n| n > 1)
+        .collect();
     v.len() as i64
 }
 
+fn collatz_length_brian(n: i64) -> i64 {
+    if n == 1 {
+        1
+    } else if n % 2 == 0 {
+        1 + collatz_length_brian(n / 2)
+    } else {
+        1 + collatz_length_brian(3 * n + 1)
+    }
+}
 
-
-fn main() -> Result<(), StrError>  {
-    let xs:Vec<i64> = (1..=5000000).collect();
-    let ys:Vec<_> = xs.iter().map(collatz_length).collect();
+fn main() -> Result<(), StrError> {
+    let xs: Vec<i64> = (1..=5000000).collect();
+    //let ys:Vec<_> = xs.iter().map(collatz_length).collect();
+    let ys: Vec<_> = xs.iter().map(|&n| collatz_length_brian(n)).collect();
 
     let mut curve = Curve::new();
     curve
         .set_label("MatPlotLib Test")
- //       .set_line_alpha(0.8)
- //       .set_line_color("#5f9cd8")
+        //       .set_line_alpha(0.8)
+        //       .set_line_color("#5f9cd8")
         .set_line_style("None")
- //       .set_marker_color("#eeea83")
+        //       .set_marker_color("#eeea83")
         .set_marker_every(1)
- //       .set_marker_line_color("#da98d1")
+        //       .set_marker_line_color("#da98d1")
         .set_marker_line_width(2.5)
         .set_marker_size(2.0)
         .set_marker_style("o");
@@ -49,20 +49,7 @@ fn main() -> Result<(), StrError>  {
     curve.draw(&xs, &ys);
     let mut plot = Plot::new();
     plot.add(&curve);
-    let _ = plot.show("myscript");
+    let _ = plot.show("myplot");
+
     Ok(())
-
-/*
-    let mut plot = Plot::new();
-    plot.set_layout(Layout::new()
-                    .title("Collatz Conjecture")
-                    .height(800)
-                    .width(1000)
-                    .x_axis(Axis::new().title("Sequence Start"))
-                    .y_axis(Axis::new().title("Sequence Length")));
-
-    let trace = Scatter::new(xs,ys).mode(Mode::Markers);
-    plot.add_trace(trace);
-    plot.show();
-*/
 }
