cf9c4308b4c44e9a72d633c909bef582d7a9340a
Eric Meyers <etm@ericmeyers.com>
2024-10-24 18:12:02 -0400

    clean

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 
 use itertools::iterate;
 use itertools::Itertools;
-use rayon::prelude::*;
 
 
 pub fn collatz_next(n: &i64) -> i64 {
@@ -27,26 +26,4 @@ pub fn collatz_length_brian(n: i64) -> i64 {
     } else {
         1 + collatz_length_brian(3 * n + 1)
     }
-}
-
-
-pub fn fivemillserial() {
-    let xs: Vec<i64> = (1..=50000).collect();
-    let ys: Vec<_>   = xs.iter().map(|&n| collatz_length_brian(n)).collect();
-}
-
-
-pub fn fivemillparallel() {
-    let xs: Vec<i64> = (1..=50000).collect();
-    let ys: Vec<_>   = xs.par_iter().map(|&n| collatz_length_brian(n)).collect();
-}
-
-
-#[cfg(test)]
-mod test {
-  use criterion::{criterion_group, criterion_main, Criterion};
-
-
-
-
 }
