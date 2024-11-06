4a9e555ea1ccb4503286f675c2cf32bf3da695a9
Brian Magaha <btmy87@gmail.com>
2024-11-05 19:30:09 -0500

    Protecting against argument < 1

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,8 @@ pub fn collatz_length(n: &i64) -> i64 {
 }
 
 pub fn collatz_length_brian(n: i64) -> i64 {
-    if n == 1 {
+    if n <= 1 {
+        // just in case some hack asks for collatz_length(0)
         1
     } else if n % 2 == 0 {
         1 + collatz_length_brian(n / 2)
