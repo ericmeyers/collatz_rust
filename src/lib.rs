712ca2637566969b3bf664572c080e307702cce3
Eric Meyers <etm@ericmeyers.com>
2024-12-01 11:17:25 -0500

    added tail call optimization

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 
 use itertools::iterate;
 use itertools::Itertools;
-
+use tailcall::tailcall;
 
 pub fn collatz_next(n: &i64) -> i64 {
     if n % 2 == 0 {
@@ -19,12 +19,20 @@ pub fn collatz_length(n: &i64) -> i64 {
 }
 
 pub fn collatz_length_brian(n: i64) -> i32 {
-    if n <= 1 {
-        // just in case some hack asks for collatz_length(0)
-        1
-    } else if n % 2 == 0 {
-        1 + collatz_length_brian(n / 2)
-    } else {
-        1 + collatz_length_brian(3 * n + 1)
+// compiler fail if not in tco
+    #[tailcall]
+    fn tco_helper(n: i64, sum: i64) -> i32 {
+        if n <= 1 {
+            // just in case some hack asks for collatz_length(0)
+            1 + sum as i32
+        } else if n % 2 == 0 {
+            tco_helper(n / 2, sum + 1)
+        } else {
+            tco_helper(3 * n + 1, sum + 1)
+        }
+
     }
+
+    tco_helper(n, 0)
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,27 @@
 use collatz_rust::collatz_length_brian;
 use plotters::prelude::*;
+use std::time::Instant;
 
 fn main()  {
 
   let high = 1000000000;
+  //let high = 100000;
 
-  let xs: Vec<i32> = (1..=high).collect();
-  let points = xs.into_iter().map(|n| {let y = collatz_length_brian(n as i64); (n,y)});
-  let root_area = BitMapBackend::new("myplot.png", (1200, 800))
+  let xs  = 1..=high;
+  let points = xs.map(|n| (n,collatz_length_brian(n)) );
+
+  let start = Instant::now();
+
+  let root_area = BitMapBackend::new("myplot.png", (1000, 600))
     .into_drawing_area();
 
   root_area.fill(&WHITE).unwrap();
-
 
   let mut ctx = ChartBuilder::on(&root_area)
     .set_label_area_size(LabelAreaPosition::Left, 40)
     .set_label_area_size(LabelAreaPosition::Bottom, 40)
     .caption("Plotters Rust", ("sans-serif", 40))
-    .build_cartesian_2d(1..high, 0..700)
+    .build_cartesian_2d(1..high, 0..900)
     .unwrap();
 
   ctx.configure_mesh().draw().unwrap();
@@ -26,6 +30,7 @@ fn main()  {
 
   ctx.draw_series(series).unwrap();
 
-
+  let duration = start.elapsed();
+  println!("Time to compute points AND plot is: {:?}", duration);
 
 }
